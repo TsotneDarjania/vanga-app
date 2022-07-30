6dfea427ca4cd73ac931de1596e62916caaf5f6b
tsotne123 <tsotne@profairgames.com>
2022-07-30 14:55:06 +0400

    completer into preload

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,41 +4,34 @@ import "./intro.css"
 import {useRef, useEffect} from 'react';
 import { useState } from 'react';
 
+
 const Intro = () => {
 
   const [startIsPossible, setStartIsPossible] = useState(false);
+  const lastTextRef = useRef();
 
-
-  const ref = useRef(null);
 
   useEffect(() => {
-    const handleClick = event => {
-      console.log('start is possible');
-      setStartIsPossible(true)
-    };
-
-    const e = ref.current;
-
-    e.addEventListener('animationend', handleClick);
-
-    return () => {
-      e.removeEventListener('animationend', handleClick);
-    };
-  }, []);
-
-
+    lastTextRef.current.addEventListener('animationend', () => {
+      setStartIsPossible(true);
+    });
+  })
 
   
   return (
     <div className='intro'>
         <div className='intro-preload'>
+
+            <div className="intro-bck-image"></div>
+            
+
             <p className='intro-txt text-show-animation'> This is a marvelous app, where you find out your future </p>
             <p className='intro-txt text-show-animation-2 intro-txt-2'> So, please approach this carefully </p>
-            <p ref={ref} className='intro-txt text-show-animation-3 intro-txt-3'> Soon you will know what awaits you in your life </p>
+            <p ref={lastTextRef} className='intro-txt text-show-animation-3 intro-txt-3'> Soon you will know what awaits you in your life </p>
             {startIsPossible ? 
             <p className='intro-txt intro-bottom-txt flashing-animation'> touch the screen to start</p>    
             : 
-            null }
+            null } 
             
         </div>
     </div>
